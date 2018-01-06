d6af8d64bf9364ee0fed70ab3724517974b28758
gongyao <gongyao@example.com>
2018-01-06 18:37:06 +0800

    [提交代码]

--- test/test.go
+++ test/test.go
@@ -5,7 +5,16 @@ import (
 	"go_socket/database"
 	"fmt"
 	"github.com/garyburd/redigo/redis"
+	"reflect"
 )
+
+type User  struct{
+	Title string `redis:"title"`
+	Author string `redis:"author"`
+	Body string `redis:"body"`
+}
+
+var p1,p2 User
 
 func main()  {
 	//server.SocketRun("10001")
@@ -13,12 +22,30 @@ func main()  {
 
 	defer redis_.Close()
 
-	str, err := redis.String(redis_.Do("GET", "bcd"))
+	p1.Title = "gongyao book"
+	p1.Author = "gyao"
+	p1.Body = "gongyao graduate from yanshan university"
 
+	_, err := redis_.Do("HMSET", redis.Args{}.Add("gongyao").AddFlat(p1)...)
 	if err != nil {
-		fmt.Println("Error %s", err.Error())
+		fmt.Printf("error %s", err.Error())
 	}
 
+	//str, err := redis.String(redis_.Do("GET", "bcd"))
+	str,err := redis.Values(redis_.Do("HGETALL", "gongyao"))
 
-	fmt.Printf("redis: %s", str)
+	if err != nil {
+		fmt.Printf("Error %s", err.Error())
+	}
+
+	redis.ScanStruct(str, &p2)
+
+	fmt.Printf("redis: %v\n", p2)
+
+	//遍历结构体
+	t := reflect.TypeOf(p2)
+	value := reflect.ValueOf(p2)
+	for i := 0; i < value.NumField(); i++ {
+		fmt.Printf("key:%s, value:%s\n", t.Field(i).Name, value.Field(i))
+	}
 }
